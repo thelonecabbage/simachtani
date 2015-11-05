d75cc9e1128f5130903cfc34e62aae07c0296673
bluevine-dev/thelonecabbage <thelonecabbage@gmail.com>
2015-11-05 11:54:28 +0200

    basic load, menu, and init

---
+++ www/js/app.js
@@ -0,0 +1,47 @@
+var app = angular.module('SimchataniApp', ['ngMaterial','ngAnimate','ui.router']);
+
+app.controller('AppController', function($mdSidenav) {
+  var vm = this;
+
+  vm.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+  this.settings = {
+      printLayout: true,
+      showRuler: true,
+      showSpellingSuggestions: true,
+      presentationMode: 'edit'
+    };
+    this.sampleAction = function(name, ev) {
+      $mdDialog.show($mdDialog.alert()
+        .title(name)
+        .content('You triggered the "' + name + '" action')
+        .ok('Great')
+        .targetEvent(ev)
+      );
+    };
+
+});
+
+app.config(function($mdIconProvider) {
+    $mdIconProvider
+      .defaultIconSet('/bower_components/angular-material/demos/icon/demoSvgIconSets/assets/core-icons.svg', 24);
+  })
+  .filter('keyboardShortcut', function($window) {
+    return function(str) {
+      if (!str) return;
+      var keys = str.split('-');
+      var isOSX = /Mac OS X/.test($window.navigator.userAgent);
+      var seperator = (!isOSX || keys.length > 2) ? '+' : '';
+      var abbreviations = {
+        M: isOSX ? '⌘' : 'Ctrl',
+        A: isOSX ? 'Option' : 'Alt',
+        S: 'Shift'
+      };
+      return keys.map(function(key, index) {
+        var last = index == keys.length - 1;
+        return last ? key : abbreviations[key];
+      }).join(seperator);
+    };
+  })
